7c80d0b8e680b29520b500c52ee02f10c7241ee5
Jeff Taylor <Jeff Taylor>
2016-11-13 21:57:38 -0800

    Add projects

---
+++ Birthday.cs
@@ -0,0 +1,68 @@
+﻿// Jeffery Taylor, BIT 142, A2.0
+using System;
+using A3_Date_StudentWork;
+
+namespace A3_Date
+{
+    class Birthday
+    {
+            
+            private MyDate today = new MyDate(0,0);
+            private int todayMonth;
+            private int todayDay;
+
+            private MyDate bday = new MyDate(0,0);
+            private int bdayMonth;
+            private int bdayDay;
+            // Initializes all the variables.
+
+        static void Main(string[] args)
+        {
+            Birthday Bday = new Birthday();
+            Bday.Questions();   // Prompts Question.
+            Bday.BirthdayCalc();// Calculates the number of days to Bday.
+        }
+
+        public void Questions() // This method promts the questions, sets the 'today' and 'bday' variables then prints how many days in the Bday month.
+        {
+            Console.WriteLine("What month is it now?");
+            Int32.TryParse(Console.ReadLine(),out todayMonth);
+
+            Console.WriteLine("What day is it now?");
+            Int32.TryParse(Console.ReadLine(), out todayDay);
+
+            Console.WriteLine("What month is your birthday in?");
+            Int32.TryParse(Console.ReadLine(), out bdayMonth);
+
+            Console.WriteLine("What day is your birthday on?");
+            Int32.TryParse(Console.ReadLine(), out bdayDay);
+
+            today.setDate(todayMonth, todayDay);// Sets the 'today'.
+            bday.setDate(bdayMonth, bdayDay);   // Sets the 'bday'.
+
+            Console.WriteLine("There are {0} days in month #{1}", bday.daysInMonth(), bday.getMonth());// Prints the number of days in the Bday month.
+        }
+
+        public void BirthdayCalc()// This method calculates the number of days to Bday.
+        {
+            int daysUntilBirthday = 0;
+
+            if (bday.equals(today))// If users Bday, prints happy bday.
+                Console.WriteLine("Happy Birthday!!! :D");
+            else// If not, then calculates how many days.
+            {
+                for (int i = 0; i < 365; i++)// Goes through a full year.
+                {
+                    if (bday.equals(today))// When its users Bday, stops the counter.
+                        break;
+                    else
+                    {
+                        today.nextDay();
+                        daysUntilBirthday++;
+                    }
+                }
+                Console.WriteLine("Number of days until birthday {0}: {1}", bday.toString(), daysUntilBirthday);// Prints number of days until Bday.
+            }
+        }
+    }
+}
---
+++ MyDate.cs
@@ -0,0 +1,115 @@
+﻿// Jeffery Taylor, BIT 142, A2.0
+using System;
+
+namespace A3_Date_StudentWork
+{
+    class MyDate
+    {
+        int month;
+        int day;
+
+            public MyDate(int m, int d) //Constructs a new MyDate representing the given month and day.
+            {
+                 month = m;
+                 day  = d;
+            }
+
+            public int getMonth() // Gets the Month.
+            {
+                return month;
+            }
+
+            public int getDay() // Gets the Day.
+            {
+                return day;
+            }
+
+            public void setDate(int m, int d) // This method changes the month and the day.
+            {
+                month = m;
+                day = d;
+            }
+
+            public String toString()  // This method returns a String representation of the MyDate object on which it was called in a month/day format.
+            {
+                return month.ToString() + "/" + day.ToString(); 
+            }
+
+            public Boolean equals(MyDate d) // This method returns true if its the users birthday and false when it is not.           
+            {
+
+                if (d.getMonth() == month && d.getDay() == day)
+                    return true;
+                else
+                    return false;
+            }
+
+            public int daysInMonth() // This method returns the number of days in the month represented by the MyDate object on which it was called.
+            {
+                int[] DinM = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+                for (int i = 0; i < DinM.Length; i++)
+                {
+                    if (i == month)
+                        return DinM[i];
+                }
+                return 0000; // Error handling
+            }
+
+            public void nextDay() // This method modifies the state of the MyDate object on which it was called by advancing it 1 day in time.
+            {
+                if (month == 2) // If Febuary.
+                {
+                    if (day == 28)
+                    {
+                        month++;
+                        day = 1;
+                    }
+                    else
+                        day++;
+                }
+
+                else if (month == 3 || month == 5 || month == 8 || month == 10) // ElseIf months have 30 days. 
+                {
+                    if (day == 30)
+                    {
+                        month++;
+                        day = 1;
+                    }
+                    else
+                        day++;
+                }
+
+                else // Else months have 31 days.
+                {
+                    if (day < 31)
+                        day++;
+                    else if (month < 12)
+                    {
+                        month++;
+                        day = 1;
+                    }
+                    else
+                    {
+                        month = 1;
+                        day = 1;
+                    }
+                }
+            }
+    }
+} 
+
+// old date arrays idea
+//int[][] MinY = {};
+                //MinY[0] = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 }; //Jan
+                //MinY[1] = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28 };             //Feb
+                //MinY[2] = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 }; //Mar
+                //MinY[3] = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30 };     //Apr
+                //MinY[4] = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 }; //May
+                //MinY[5] = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30 };     //Jun
+                //MinY[6] = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 }; //Jul
+                //MinY[7] = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 }; //Aug
+                //MinY[8] = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30 };     //Sep
+                //MinY[9] = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 }; //Oct
+                //MinY[10] = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30 };    //Nov
+                //MinY[11] = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 };//Dec
